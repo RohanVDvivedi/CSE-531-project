5fa3aeaa7f8e39f60f5eb935cff7cbc8750218a8
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-28 08:55:27 -0700

    compiled outputs of protobuf files

--- branch_pb2.pyi
+++ branch_pb2.pyi
@@ -5,15 +5,23 @@ from typing import ClassVar as _ClassVar, Optional as _Optional
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class Request(_message.Message):
-    __slots__ = ["money"]
+    __slots__ = ["customer_request_id", "logical_clock", "money"]
+    CUSTOMER_REQUEST_ID_FIELD_NUMBER: _ClassVar[int]
+    LOGICAL_CLOCK_FIELD_NUMBER: _ClassVar[int]
     MONEY_FIELD_NUMBER: _ClassVar[int]
+    customer_request_id: int
+    logical_clock: int
     money: int
-    def __init__(self, money: _Optional[int] = ...) -> None: ...
+    def __init__(self, customer_request_id: _Optional[int] = ..., logical_clock: _Optional[int] = ..., money: _Optional[int] = ...) -> None: ...
 
 class Response(_message.Message):
-    __slots__ = ["success", "balance"]
+    __slots__ = ["customer_request_id", "logical_clock", "success", "balance"]
+    CUSTOMER_REQUEST_ID_FIELD_NUMBER: _ClassVar[int]
+    LOGICAL_CLOCK_FIELD_NUMBER: _ClassVar[int]
     SUCCESS_FIELD_NUMBER: _ClassVar[int]
     BALANCE_FIELD_NUMBER: _ClassVar[int]
+    customer_request_id: int
+    logical_clock: int
     success: bool
     balance: int
-    def __init__(self, success: bool = ..., balance: _Optional[int] = ...) -> None: ...
+    def __init__(self, customer_request_id: _Optional[int] = ..., logical_clock: _Optional[int] = ..., success: bool = ..., balance: _Optional[int] = ...) -> None: ...
