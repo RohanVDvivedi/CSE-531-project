96d703bcfa95f2601dc94193df0f838b4eb919f8
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-31 21:43:52 -0700

    implemented event processed list for branch

--- Branch.py
+++ Branch.py
@@ -13,13 +13,14 @@ class Branch(branch_pb2_grpc.BranchServicer):
         # replica of the Branch's balance
         self.balance = balance
         # the list of process IDs of the branches
-        self.branches = branches
         # the list of Client stubs to communicate with the branches
         self.channels = list()
         self.stubList = list()
+        self.branches = list()
         for branch in branches :
             if branch == self.id :
                 continue
+            self.branches.append(branch)
             self.channels.append(grpc.insecure_channel("localhost:" + str(50000 + branch)))
             self.stubList.append(branch_pb2_grpc.BranchStub(self.channels[-1]))
         # logical clock
@@ -44,15 +45,18 @@ class Branch(branch_pb2_grpc.BranchServicer):
     # TODO: students are expected to process requests from both Client and Branch
     def Query(self, request, context):
         recv_time = self.advance_and_get_logical_clock(request.logical_clock)
+        self.event_processed.append({"customer-request-id": request.customer_request_id, "logical_clock": recv_time, "interface": "query", "comment":"event_recv from customer " + str(self.id)})
         balance = self.balance
         send_time = self.advance_and_get_logical_clock()
         return branch_pb2.Response(customer_request_id = request.customer_request_id, logical_clock = send_time, balance = balance)
     
     def Withdraw(self, request, context):
         recv_time = self.advance_and_get_logical_clock(request.logical_clock)
+        self.event_processed.append({"customer-request-id": request.customer_request_id, "logical_clock": recv_time, "interface": "withdraw", "comment":"event_recv from customer " + str(self.id)})
         self.balance -= request.money
-        for branch in self.stubList :
+        for branch_id, branch in zip(self.branches, self.stubList) :
             send_time = self.advance_and_get_logical_clock()
+            self.event_processed.append({"customer-request-id": request.customer_request_id, "logical_clock": send_time, "interface": "propogate_withdraw", "comment":"event_sent to branch " + str(branch_id)})
             resp = branch.Propogate_Withdraw(branch_pb2.Request(customer_request_id = request.customer_request_id, logical_clock = send_time))
             recv_time = self.advance_and_get_logical_clock(resp.logical_clock)
         send_time = self.advance_and_get_logical_clock()
@@ -60,9 +64,11 @@ class Branch(branch_pb2_grpc.BranchServicer):
     
     def Deposit(self, request, context):
         recv_time = self.advance_and_get_logical_clock(request.logical_clock)
+        self.event_processed.append({"customer-request-id": request.customer_request_id, "logical_clock": recv_time, "interface": "deposit", "comment":"event_recv from customer " + str(self.id)})
         self.balance += request.money
-        for branch in self.stubList :
+        for branch_id, branch in zip(self.branches, self.stubList) :
             send_time = self.advance_and_get_logical_clock()
+            self.event_processed.append({"customer-request-id": request.customer_request_id, "logical_clock": send_time, "interface": "propogate_deposit", "comment":"event_sent to branch " + str(branch_id)})
             resp = branch.Propogate_Deposit(branch_pb2.Request(customer_request_id = request.customer_request_id, logical_clock = send_time))
             recv_time = self.advance_and_get_logical_clock(resp.logical_clock)
         send_time = self.advance_and_get_logical_clock()
